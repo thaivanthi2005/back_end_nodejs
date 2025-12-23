830d782bb6591188101d10fa6a628c6a494e31f9
thaivanthi2005 <thaivanthi2005@gmail.com>
2025-12-24 00:21:34 +0700

    xong kết nối mongoDB

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const route = require("./routes/client/index.route");
+const { Mongoose, default: mongoose } = require("mongoose");
 
 const port = 3000;
+
+mongoose.connect("mongodb://127.0.0.1:27017/project1");
 
 app.set("views", "./views");
 app.set("view engine", "pug");
