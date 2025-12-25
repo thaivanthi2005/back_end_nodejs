863e89e7255706a8cc3e1b1bdaeaac3b2aa02d32
thaivanthi2005 <thaivanthi2005@gmail.com>
2025-12-25 21:50:26 +0700

    làm trang danh sách ( kết nối DTB để hiện lên danh sách )

--- controller/client/products.controller.js
+++ controller/client/products.controller.js
@@ -1,5 +1,18 @@
-module.exports.products = (req, res) => {
+const Product = require("../../models/products.model");
+module.exports.products = async (req, res) => {
+  const products2 = await Product.find({
+    status: "In Stock",
+    delete: false,
+  });
+  const newProduct = products2.map((item) => {
+    item.pricenew = (
+      (item.price * (100 - item.discoutprecentage)) /
+      100
+    ).toFixed(0);
+    return item;
+  });
   res.render("client/pages/products/index", {
     pagetitle: "Danh sách sản phẩm",
+    products: newProduct,
   });
 };
