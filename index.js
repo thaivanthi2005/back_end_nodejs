3edec80a3fde5444ea4b69dfb55b21bf7cdcc407
THÁI VĂN THI <thaivanthi2005@gmail.com>
2025-12-26 22:45:15 +0700

    Build layout chung admin

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const app = express();
 
 const route = require("./routes/client/index.route");
 const route_admin = require("./routes/admin/index.route");
-
 const database = require("./config/database");
 const port = process.env.PORT;
+const system_config = require("./config/system");
 
 app.set("views", "./views");
 app.set("view engine", "pug");
 app.use(express.static("public"));
+
+app.locals.prefixAdmin = system_config.prefixAdmin;
 
 route(app);
 route_admin(app);
