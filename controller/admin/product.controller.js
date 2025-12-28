46b7a4fc3feb605dd9197d002541f2c7533ee2db
THÁI VĂN THI <thaivanthi2005@gmail.com>
2025-12-28 22:48:13 +0700

    Build Lọc theo trạng thái _ admin

--- controller/admin/product.controller.js
+++ controller/admin/product.controller.js
@@ -2,11 +2,45 @@
 const Product = require("../../models/products.model");
 
 module.exports.index = async (req, res) => {
-  const products2 = await Product.find({
+  // console.log(req.query);
+  let category = [
+    {
+      name: "Tất cả",
+      status: "",
+      class: "",
+    },
+    {
+      name: "Còn hàng",
+      status: "InStock",
+      class: "",
+    },
+    {
+      name: "Hết hàng",
+      status: "OutStock",
+      class: "",
+    },
+  ];
+
+  if (req.query.status) {
+    const index = category.findIndex((item) => item.status == req.query.status);
+    category[index].class = "active";
+  } else {
+    const index = category.findIndex((item) => item.status == "");
+    category[index].class = "active";
+  }
+
+  let find = {
     delete: false,
-  });
+  };
+
+  if (req.query.status) {
+    find.status = req.query.status;
+  }
+
+  const products2 = await Product.find(find);
   res.render("admin/pages/products/index", {
     pagetitle: "Trang sản phẩm",
     products: products2,
+    category: category,
   });
 };
