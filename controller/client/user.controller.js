e79beb6c5096bcebbeb115bcde1a4a2190a5bdb0
THÁI VĂN THI <thaivanthi2005@gmail.com>
2026-06-08 15:46:04 +0700

    done login user

--- controller/client/user.controller.js
+++ controller/client/user.controller.js
@@ -5,7 +5,7 @@ const User = require("../../models/user.model");
 var md5 = require("md5");
 
 //[GET] /user/register
-module.exports.index = async (req, res) => {
+module.exports.indexRegister = async (req, res) => {
   res.render("client/pages/user/register", {
     pagetitle: "Đăng Ký",
   });
@@ -17,12 +17,46 @@ module.exports.registerPost = async (req, res) => {
   });
   if (existEmail) {
     req.session.error = ["EMAIL ĐÃ TỒN TẠI"];
-    res.redirect("back");
+    res.redirect(req.get("referer"));
     return;
   }
   req.body.password = md5(req.body.password);
   const user = new User(req.body);
   await user.save();
   res.cookie("tokenUser", user.tokenUser);
-  res.redirect(req.get("referer"));
+  res.redirect(`/`);
+};
+
+//[GET] /user/login
+module.exports.indexLogin = async (req, res) => {
+  res.render("client/pages/user/login", {
+    pagetitle: "Đăng Nhập",
+  });
+};
+// [POST] /user/register
+module.exports.loginPost = async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const user = await User.findOne({
+    email: email,
+    deleted: false,
+  });
+  if (!user) {
+    req.session.error = ["EMAIL KO TỒN TẠI"];
+    res.redirect(req.get("referer"));
+    return;
+  }
+  if (md5(password) != user.password) {
+    req.session.error = ["SAI MẬT KHẨU"];
+    res.redirect(req.get("referer"));
+    return;
+  }
+  if (user.password === "inactive") {
+    req.session.error = ["TÀI KHOẢN ĐÃ BỊ KHÓA "];
+    res.redirect(req.get("referer"));
+    return;
+  }
+  res.cookie("tokenUser", user.tokenUser);
+
+  res.redirect(`/`);
 };
--- validate/client/user.validate.js
+++ validate/client/user.validate.js
@@ -19,3 +19,18 @@ module.exports.registerPost = (req, res, next) => {
 
   next();
 };
+module.exports.loginPost = (req, res, next) => {
+  if (!req.body.email) {
+    req.session.error = ["Vui lòng nhập email!"];
+    res.redirect(req.get("referer"));
+    return;
+  }
+
+  if (!req.body.password) {
+    req.session.error = ["Vui lòng nhập mật khẩu!"];
+    res.redirect(req.get("referer"));
+    return;
+  }
+
+  next();
+};
