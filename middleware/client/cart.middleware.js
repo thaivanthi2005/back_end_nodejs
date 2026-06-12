f409302dc14278b922ba5f651dc08929cc331490
THÁI VĂN THI <thaivanthi2005@gmail.com>
2026-06-12 23:51:55 +0700

    fix : null cart crash on production

--- middleware/client/cart.middleware.js
+++ middleware/client/cart.middleware.js
@@ -1,5 +1,5 @@
 const Cart = require("../../models/cart.model");
-const system_config = require("../../config/system");
+
 module.exports.checkcart = async (req, res, next) => {
   if (!req.cookies.cartId) {
     const cart = new Cart();
@@ -9,14 +9,23 @@ module.exports.checkcart = async (req, res, next) => {
       expires: new Date(Date.now() + expiresCookie),
     });
   } else {
-    const cart_pro = await Cart.findOne({
-      _id: req.cookies.cartId,
-    });
+    const cart_pro = await Cart.findOne({ _id: req.cookies.cartId });
 
-    const sum_quantity = cart_pro.products.reduce((total, curvalue) => {
-      return total + curvalue.quantity;
-    }, 0);
-    res.locals.total_quantity = sum_quantity;
+    // ✅ Thêm check null
+    if (!cart_pro) {
+      const cart = new Cart();
+      await cart.save();
+      const expiresCookie = 365 * 24 * 60 * 60 * 1000;
+      res.cookie("cartId", cart.id, {
+        expires: new Date(Date.now() + expiresCookie),
+      });
+      res.locals.total_quantity = 0;
+    } else {
+      const sum_quantity = cart_pro.products.reduce((total, curvalue) => {
+        return total + curvalue.quantity;
+      }, 0);
+      res.locals.total_quantity = sum_quantity;
+    }
   }
   next();
 };
