78037c5695b364a4b1647ff3e6a36a7c04a2abb2
THÁI VĂN THI <thaivanthi2005@gmail.com>
2026-06-14 18:21:43 +0700

    view data realtime

--- controller/client/chat.controller.js
+++ controller/client/chat.controller.js
@@ -4,6 +4,7 @@ const User = require("../../models/user.model");
 const Chat = require("../../models/chat.model");
 module.exports.index = async (req, res) => {
   const user_id = res.locals.user.id;
+  const fullName = res.locals.user.fullName;
   // SocketIo
   _io.once("connection", async (socket) => {
     console.log("a user connected", socket.id);
@@ -14,6 +15,13 @@ module.exports.index = async (req, res) => {
         content: content,
       });
       await chat.save();
+
+      //Trả data về client
+      _io.emit("SERVER_RETURN_MESSAGE", {
+        fullName: fullName,
+        userId: user_id,
+        content: content,
+      });
     });
   });
 
@@ -25,9 +33,8 @@ module.exports.index = async (req, res) => {
     const infoUser = await User.findOne({
       _id: chat.user_id,
     }).select("fullName");
-    chats.infoUser = infoUser;
+    chat.infoUser = infoUser;
   }
-  console.log(chats);
   // End SocketIo
   res.render("client/pages/chat/index", {
     pagetitle: "Chat",
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,4 +1,4 @@
-//CLIENT_SEND_MESSAGE
+// CLIENT_SEND_MESSAGE
 const formSendData = document.querySelector(".chat .inner-form");
 if (formSendData) {
   formSendData.addEventListener("submit", (e) => {
@@ -10,4 +10,32 @@ if (formSendData) {
     }
   });
 }
-//END CLIENT_SEND_MESSAGE
+// END CLIENT_SEND_MESSAGE
+
+// SERVER_RETURN_MESSAGE
+socket.on("SERVER_RETURN_MESSAGE", (data) => {
+  const myId = document.querySelector("[my-id]").getAttribute("my-id"); // ✅ sửa
+  const body = document.querySelector(".chat .inner-body");
+  const div = document.createElement("div");
+  let htmlFullname = "";
+  if (myId == data.userId) {
+    div.classList.add("inner-outgoing"); // ✅ sửa outcoming → outgoing
+  } else {
+    htmlFullname = `<div class="inner-name">${data.fullName}</div>`;
+    div.classList.add("inner-incoming");
+  }
+  div.innerHTML = `
+    ${htmlFullname}
+    <div class="inner-content">${data.content}</div>
+  `;
+  body.appendChild(div);
+  body.scrollTop = body.scrollHeight;
+});
+// END SERVER_RETURN_MESSAGE
+
+// Scroll Chat To Bottom
+const bodyChat = document.querySelector(".chat .inner-body");
+if (bodyChat) {
+  bodyChat.scrollTop = bodyChat.scrollHeight;
+}
+// End Scroll Chat To Bottom
