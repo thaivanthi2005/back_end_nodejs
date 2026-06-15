31765b829aacf0cd6f9732392ee20e6ee32d3612
THÁI VĂN THI <thaivanthi2005@gmail.com>
2026-06-16 02:18:11 +0700

    [chat] upload ảnh (1)

--- controller/client/chat.controller.js
+++ controller/client/chat.controller.js
@@ -2,17 +2,27 @@ const Product = require("../../models/products.model");
 const Category = require("../../models/category.model");
 const User = require("../../models/user.model");
 const Chat = require("../../models/chat.model");
+
+const uploadToCloudinary = require("../../helper/uploadToCloudinary");
+
 module.exports.index = async (req, res) => {
   const user_id = res.locals.user.id;
   const fullName = res.locals.user.fullName;
   // SocketIo
   _io.once("connection", async (socket) => {
     console.log("a user connected", socket.id);
-    socket.on("CLIENT_SEND_MESSAGE", async (content) => {
+    socket.on("CLIENT_SEND_MESSAGE", async (data) => {
+      let images = [];
+      for (const imagebuffer of data.images) {
+        const link = await uploadToCloudinary(imagebuffer);
+        images.push(link);
+      }
+      console.log(images);
       //Lưu vào DATABASE
       const chat = new Chat({
         user_id: user_id,
-        content: content,
+        content: data.content,
+        images: images,
       });
       await chat.save();
 
@@ -20,7 +30,8 @@ module.exports.index = async (req, res) => {
       _io.emit("SERVER_RETURN_MESSAGE", {
         fullName: fullName,
         userId: user_id,
-        content: content,
+        content: data.content,
+        images: images,
       });
     });
     //TYPING
--- middleware/upload.middleware.js
+++ middleware/upload.middleware.js
@@ -1,35 +1,63 @@
-const cloudinary = require("cloudinary").v2;
-const streamifier = require("streamifier");
+// const cloudinary = require("cloudinary").v2;
+// const streamifier = require("streamifier");
 
-cloudinary.config({
-  cloud_name: process.env.cloud_name,
-  api_key: process.env.api_key,
-  api_secret: process.env.api_secret,
-});
+// cloudinary.config({
+//   cloud_name: process.env.cloud_name,
+//   api_key: process.env.api_key,
+//   api_secret: process.env.api_secret,
+// });
 
-module.exports.upload_images = (req, res, next) => {
+// // module.exports.upload_images = (req, res, next) => {
+// //   if (req.file) {
+// //     let streamUpload = (req) => {
+// //       return new Promise((resolve, reject) => {
+// //         let stream = cloudinary.uploader.upload_stream((error, result) => {
+// //           if (result) {
+// //             resolve(result);
+// //           } else {
+// //             reject(error);
+// //           }
+// //         });
+
+// //         streamifier.createReadStream(req.file.buffer).pipe(stream);
+// //       });
+// //     };
+
+// //     async function upload(req) {
+// //       let result = await streamUpload(req);
+// //       req.body[req.file.fieldname] = result.url;
+// //       next();
+// //     }
+// //     upload(req);
+// //   } else {
+// //     next();
+// //   }
+// // };
+
+// //END CLOUDUNARY
+
+// let streamUpload = (buffer) => {
+//   return new Promise((resolve, reject) => {
+//     let stream = cloudinary.uploader.upload_stream((error, result) => {
+//       if (result) {
+//         resolve(result);
+//       } else {
+//         reject(error);
+//       }
+//     });
+//     streamifier.createReadStream(buffer).pipe(stream);
+//   });
+// };
+
+// const uploadToCloudinary = async (buffer) => {
+//   let result = await streamUpload(buffer);
+//   return result.url;
+// };
+const uploadToCloudinary = require("../helper/uploadToCloudinary");
+module.exports.upload_images = async (req, res, next) => {
   if (req.file) {
-    let streamUpload = (req) => {
-      return new Promise((resolve, reject) => {
-        let stream = cloudinary.uploader.upload_stream((error, result) => {
-          if (result) {
-            resolve(result);
-          } else {
-            reject(error);
-          }
-        });
-
-        streamifier.createReadStream(req.file.buffer).pipe(stream);
-      });
-    };
-
-    async function upload(req) {
-      let result = await streamUpload(req);
-      req.body[req.file.fieldname] = result.url;
-      next();
-    }
-    upload(req);
-  } else {
-    next();
+    const link = await uploadToCloudinary(req.file.buffer);
+    req.body[req.file.fieldname] = link;
   }
+  next();
 };
