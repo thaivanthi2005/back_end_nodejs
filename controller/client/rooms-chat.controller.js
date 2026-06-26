46cd86a356c26d2fc8a4919114228487d4b29083
THÁI VĂN THI <thaivanthi2005@gmail.com>
2026-06-27 00:02:23 +0700

    create rooms-chat

--- controller/client/rooms-chat.controller.js
+++ controller/client/rooms-chat.controller.js
@@ -1,10 +1,49 @@
 const system_config = require("../../config/system");
-const Product = require("../../models/products.model");
-const helper = require("../../helper/pricenew");
-
+const User = require("../../models/user.model");
+const RoomChat = require("../../models/rooms-chat.model");
 //[GET] /
 module.exports.index = async (req, res) => {
   res.render("client/pages//rooms-chat/index", {
     pagetitle: "Phòng Chat",
   });
+};
+
+//GET /create
+module.exports.create = async (req, res) => {
+  const friendList = res.locals.user.friendList;
+  for (const friend of friendList) {
+    const infoFriend = await User.findOne({
+      _id: friend.user_id,
+      deleted: false,
+    }).select("fullName avatar");
+    friend.infoFriend = infoFriend;
+  }
+  res.render("client/pages//rooms-chat/create", {
+    pagetitle: "Tạo Phòng",
+    friendList: friendList,
+  });
+};
+
+//POST /create
+module.exports.create_post = async (req, res) => {
+  const title = req.body.title;
+  const usersId = req.body.usersId;
+  const dataRoom = {
+    title: title,
+    typeRoom: "group",
+    users: [],
+  };
+  for (const userId of usersId) {
+    dataRoom.users.push({
+      user_id: userId,
+      role: "user",
+    });
+  }
+  dataRoom.users.push({
+    user_id: res.locals.user.id,
+    role: "superAdmin",
+  });
+  const roomChat = new RoomChat(dataRoom);
+  await roomChat.save();
+  res.redirect(`/chat/${roomChat.id}`);
 };
--- routes/client/rooms-chat.route.js
+++ routes/client/rooms-chat.route.js
@@ -2,5 +2,7 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../../controller/client/rooms-chat.controller");
 router.get("/", controller.index);
+router.get("/create", controller.create);
+router.post("/create", controller.create_post);
 
 module.exports = router;
