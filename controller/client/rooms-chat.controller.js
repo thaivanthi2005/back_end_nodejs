0e649f213bb39f1bc84c73fc17580db34cb28130
THÁI VĂN THI <thaivanthi2005@gmail.com>
2026-06-27 00:12:16 +0700

    view list rooms-chat

--- controller/client/rooms-chat.controller.js
+++ controller/client/rooms-chat.controller.js
@@ -3,8 +3,15 @@ const User = require("../../models/user.model");
 const RoomChat = require("../../models/rooms-chat.model");
 //[GET] /
 module.exports.index = async (req, res) => {
+  const userId = res.locals.user.id;
+  const listRoomChat = await RoomChat.find({
+    "users.user_id": userId,
+    typeRoom: "group",
+    deleted: false,
+  });
   res.render("client/pages//rooms-chat/index", {
     pagetitle: "Phòng Chat",
+    listRoomChat: listRoomChat,
   });
 };
 
